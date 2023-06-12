d631aa7d8e9358d8ea7dd272e4647b3601f8c0c1
Aleksandr <shbragnikov@inbox.ru>
2023-06-12 12:51:43 +0300

    4

--- programmax.java
+++ programmax.java
@@ -3,13 +3,10 @@
 import java.util.ArrayList;
 
 public class programmax {
-    //public static ArrayList<Integer> arrayMult(int i, int n, int max) {
-    public static ArrayList<Integer> arrayNotMult(int i, int n, int min) {   
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        //for (int j = i + 1; j <= max; ++j) {
-        //if (j % n == 0) {
-        for (int j = i - 1; j >= min; j--) {
-        if (j % n != 0) {
+    public static ArrayList<Integer> arrayMult(int i, int n, int max) {
+            ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int j = i + 1; j <= max; ++j) {
+        if (j % n == 0) {
             list.add(j);
             }
         }
